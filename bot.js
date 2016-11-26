8a037ff96107f52411320c681a2ff1a6e6f1a230
ryancorrao17 <ryancorrao17@solonschools.net>
2016-11-26 00:18:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,15 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
+  var aliveReq = /^\/alive?$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
+    this.res.end();
+  } else if request.text && aliveReq.test(request.text) {
+    this.res.writeHead(200);
+    postAlive();
     this.res.end();
   } else {
     console.log("don't care");
@@ -22,6 +27,41 @@ function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postAlive() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "testing zombot";
 
   options = {
     hostname: 'api.groupme.com',

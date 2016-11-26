3c097fd48abe10fe8f42249ac451dda2b7f4e682
ryancorrao17 <ryancorrao17@solonschools.net>
2016-11-26 00:32:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
-  var aliveReq = /^\/alive?$/;
+  var aliveReq = /^\/alive$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
